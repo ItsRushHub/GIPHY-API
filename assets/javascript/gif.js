c96cdeea6e8db96763c79871daf347284d1e6eb2
Oran Rush <oranrush@Orans-MacBook-Pro.local>
2019-01-16 18:17:12 -0600

    Upload2

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -4,32 +4,75 @@ var actorsArray = ["Will Smith", "Denzel Washington", "Sanaa Lathan", "Kerry Was
 
 var topics = [];
 
+// Get the gifs to animate, still and play again
+$(document).on("click", ".animal-image", function() {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
+
 // When you click on a gif button the giphys show up
-$("button[data-person]").on("click", function() {
-    var person = $(this).attr("data-person");
+$("button[data-still]").on("click", function() {
+    var person = $(this).attr("data-still");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     person + "&api_key=svAHRzuiCCv7R8Ek8WHJIt8nphDzAa1R";
 
+
     $.ajax({
-    url: queryURL,
-    method: "GET"
+        url: queryURL,
+        method: "GET"
     })
 
 // My for loop that appends a button for each string in the array
 
-    for (var i = 0; i < actorsArray.length; i++) {
-        var gifDiv = $("<div>");
-    }
+    .then(function(response) {
+        var results = response.data;
+
+
+        for (var i = 0; i < actorsArray.length; i++) {
+            var gifDiv = $("<div>");
+
+            // The ratings for the gifs
+
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+
+
+            var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height.url);
+
+            var animated = results[i].images.fixed_height.url;
+            var still = results[i].images.fixed_height_still.url;
+
+            personImage.attr("src", still);
+            personImage.attr("data-still", still);
+            personImage.attr("data-animate", animated);
+            personImage.attr("data-state", "still");
+            personImage.addClass("animal-image");
+
+
+            // Get the new gif to populate to the top
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+
+            $("#gifs-appear-here").prepend(gifDiv);
+
+        }
 
 
 
 
 
+    });
 
 
-
-
-
-
-
-
+});
